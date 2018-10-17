6d678c1e733d390f5907135102591b6ad66c5dcf
LinkToLinkHH <40762209+LinkToLinkHH@users.noreply.github.com>
2018-10-17 14:00:36 +0100

    NEA update

--- NEA.py
+++ NEA.py
@@ -2,13 +2,16 @@ import random
 Lives = 2
 password = "Lumagar"
 
-print("Welcome to the music quiz!")
-player1 = input("What is your name?")
-player1pw = input("What is the password?")
-if player1pw == password:
-    print("You can play the music quiz!!")
+while Lives != "0":
+    print("Welcome to the music quiz!")
+    player1 = input("What is your name?")
+    player1pw = input("What is the password?")
+    if player1pw == password:
+        print("You can play the music quiz!!")
 
-else:
-    print("You are not authorised to play this quiz!!")
+    else:
+        print("You are not authorised to play this quiz!!")
 
-songs = open("Songs and artists.txt")
+    Songs = open("Songstxt")
+    Songs = Songs.read()
+    Artists = open("Artist.txt")
