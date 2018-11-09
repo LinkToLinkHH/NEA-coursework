1e731358b109e2138d0fa2aeb78c675c4453f714
LinkToLinkHH <40762209+LinkToLinkHH@users.noreply.github.com>
2018-11-09 22:51:54 +0000

    Update

--- NEA.py
+++ NEA.py
@@ -3,14 +3,29 @@ Lives = 2
 password = "Lumagar"
 
 print("Welcome to the music quiz!")
-player = input("What is your name?")
-playerpw = input("What is the password?")
+player = str(input("What is your name?"))
+playerpw = str(input("What is the password?"))
 if playerpw == password:
     print("You can play the music quiz!!")
 
 else:
     print("You are not authorised to play this quiz!!")
 
+correctanswer = True
+def correct():
+    if correctanswer == True:
+        points = points +3
+
+def incorrect():
+    correctanswer = False
+    if correctanswer == False:
+        Lives = Lives - 1
+
+    def lives():
+        if lives == 2:
+            points = points + 3
+        elif lives == 1:
+            points = points + 1
 
 def remove():
     test = items[i].replace("A","-")
@@ -23,6 +38,8 @@ def remove():
 
     test3 = test2.replace("U","-")
     print(test3)
+
+    
 def choose_songs():
     Songs = open("Songs.txt""r")
     Songs = Songs.read()
@@ -33,7 +50,18 @@ def choose_songs():
     Artists = Artists.read()
     items = Artists.split(',')
     remove()
+    if Songs != Songs:
+        correct() or incorrect()
 
+    
     print("Let's start!!")
     choose_song()
+
+
+if Lives == 0:
+    print("You have no more lives left!")
+    Player = open("Players.txt" "a")
+    Player = Players.txt.append(player)
+
+
     
